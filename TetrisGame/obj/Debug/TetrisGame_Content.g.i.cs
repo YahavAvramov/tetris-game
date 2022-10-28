235a56139922fd4924e5dd489a0615625d9e413f
Yahav <109096266+YahavAvramov@users.noreply.github.com>
2022-10-28 16:59:43 +0300

    chage background

--- TetrisGame/obj/Debug/TetrisGame_Content.g.i.cs
+++ TetrisGame/obj/Debug/TetrisGame_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/backgroundimg.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/background1.jpg")]
 
 
